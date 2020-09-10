3d8f7943b06d3cf1e7c521ab949bce5a45fd3219
Tanuj Jain <tanuj.jain@somaiya.edu>
2020-09-10 10:46:01 +0530

    AI updated

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ const combinations = [
 const human = 'X'
 const ai = 'O'
 let currPlayer = human
-let clicks = 0
 let gameEnd = false
 
 
@@ -24,12 +23,12 @@ cells.forEach((cell) => {
     cell.addEventListener('click', () => {
         if (cell.innerText === '') {
             cell.innerText = currPlayer
-            clicks += 1
             gameLoop()
             if (!gameEnd) {
                 setTimeout(() => {
-                    aiPlay()
-                }, 500);
+                    // aiRandomPlay()
+                    aiMinimaxPlay()
+                }, 100)
             }
         }
     })
@@ -41,20 +40,16 @@ restartButon.addEventListener('click', () => {
 
 const gameLoop = () => {
     const winner = checkWin()
-    console.log(winner)
     if (winner !== null) {
         endGame(winner)
     }
     changePlayer()
 }
 
-const aiPlay = () => {
-    const available = cells.filter((cell) => {
-        return cell.innerText === ''
-    })
+const aiRandomPlay = () => {
+    const available = emptyCells()
     if (available.length !== 0) {
         available[Math.floor(Math.random() * available.length)].innerText = currPlayer
-        clicks += 1
         gameLoop()
     }
 }
@@ -69,12 +64,17 @@ const changePlayer = () => {
 
 const checkWin = () => {
     let winner = null
+    const available = emptyCells()
     combinations.forEach((arr) => {
-        if (cells[arr[0]].innerText === currPlayer && cells[arr[1]].innerText === currPlayer && cells[arr[2]].innerText === currPlayer) {
-            winner = currPlayer
+        if (cells[arr[0]].innerText === cells[arr[1]].innerText && cells[arr[1]].innerText === cells[arr[2]].innerText) {
+            if (cells[arr[0]].innerText === human) {
+                winner = human
+            } else if (cells[arr[0]].innerText === ai) {
+                winner = ai
+            }
         }
     })
-    if (winner === null && clicks === 9) {
+    if (winner === null && available.length === 0) {
         winner = 'T'
     }
     return winner
@@ -93,10 +93,75 @@ const endGame = (shape) => {
 
 const resetGame = () => {
     currPlayer = human
-    clicks = 0
     gameEnd = false
     cells.forEach(cell => {
         cell.innerText = ''
     })
     endScreen.classList.remove('show')
+}
+
+const emptyCells = () => {
+    const available = cells.filter((cell) => {
+        return cell.innerText === ''
+    })
+    return available
+}
+
+const scores = {
+    'X': -10,
+    'O': +10,
+    'T': 0
+}
+
+const aiMinimaxPlay = () => {
+    const available = emptyCells()
+    let bestScore = -Infinity
+    let bestMove
+    for (let i = 0; i < available.length; i++) {
+        available[i].innerText = currPlayer
+        const score = minimax(false, 0, -Infinity, Infinity)
+        available[i].innerText = ''
+        if (score > bestScore) {
+            bestScore = score
+            bestMove = available[i]
+        }
+    }
+    bestMove.innerText = ai
+    gameLoop()
+}
+
+const minimax = (maxTurn, depth, alpha, beta) => {
+    let winner = checkWin()
+    if (winner !== null) {
+        return scores[winner]
+    }
+    if (maxTurn) {
+        const available = emptyCells()
+        let bestScore = -Infinity
+        for (let i = 0; i < available.length; i++) {
+            available[i].innerText = ai
+            const score = minimax(false, depth + 1, alpha, beta)
+            available[i].innerText = ''
+            bestScore = Math.max(bestScore, score)
+            alpha = Math.max(alpha, bestScore)
+            if (alpha >= beta) {
+                break
+            }
+        }
+        return bestScore
+    } else {
+        const available = emptyCells()
+        let bestScore = Infinity
+        for (let i = 0; i < available.length; i++) {
+            available[i].innerText = human
+            const score = minimax(true, depth + 1, alpha, beta)
+            available[i].innerText = ''
+            bestScore = Math.min(bestScore, score)
+            beta = Math.min(beta, bestScore)
+            if (beta <= alpha) {
+                break
+            }
+        }
+        return bestScore
+    }
 }
